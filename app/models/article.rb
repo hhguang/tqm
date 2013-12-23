80559bfed8cadeb07bc5dd4f8929b8e0cb3ff5d6
hhguang <hhguang@gmail.com>
2013-12-23 17:50:02 +0800

    topic and attachments

--- app/models/article.rb
+++
@@ -1,9 +0,0 @@
-class Article < ActiveRecord::Base
-	belongs_to :exam
-	has_many	:attachments, :dependent => :destroy
-	
-	validates :title ,:presence => true
-
-	accepts_nested_attributes_for :attachments, :reject_if => lambda { |a| a[:file].blank? }, :allow_destroy => true
-
-end
