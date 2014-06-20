09e5fa0c068e9d2ccee2b4b166dfb36bd8566374
vagrant <vagrant@precise32.(none)>
2014-06-20 08:40:26 +0000

    small_score set scoring_average in 0-100

--- app/models/small_score.rb
+++ app/models/small_score.rb
@@ -1,5 +1,7 @@
 class SmallScore < ActiveRecord::Base
   belongs_to :exam
   validates :average, :numericality=>{:greater_than_or_equal_to=>0}, allow_nil: true
-  validates :scoring_average,:numericality=>{:greater_than_or_equal_to=>0}, allow_nil: true
+  validates :scoring_average,
+    :numericality=>{greater_than_or_equal_to: 0, less_than_or_equal_to: 100},
+    allow_nil: true
 end
